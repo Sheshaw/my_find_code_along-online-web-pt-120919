657dc1648759d3342794e18a08596cacc32ad0a5
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-23 03:06:45 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
-def my_find(collection)
+collection = (1..100).to_a
 
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
 end
+
+puts my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+def other_find(collection)
+  array = []
+  i = 0
+  while i < collection.length
+    array.push(collection[i]) if yield(collection[i])
+    i = i + 1
+  end
+  array
+end
+
+
+puts other_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
